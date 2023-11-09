e0a54a1dd9985d84c69afef655981c479d8608b9
Vyacheslav Chernyak <Vachernyak@mts.ru>
2023-11-09 09:28:39 +0300

    Added HW

--- INSERT.sql
+++ INSERT.sql
@@ -1,3 +1,4 @@
+---------------------TASK №1--------------------
 insert into musical_genre (name) values 
 ('JAZ'),
 ('BLUES'),
@@ -5,7 +6,7 @@ insert into musical_genre (name) values
 
 insert into artist (name) values 
 ('Louis Armstrong'),
-('Duke Ellington'),
+('Duke'),
 ('James Marshall (Jimi) Hendrix'),
 ('Robert William Gary Moore');
 
@@ -15,11 +16,11 @@ insert into artist_genre (artist_id, genre_id) values
 (3,3),
 (4,2);
 
-insert into album  (name, year_of_issue) values 
+insert into album  (album_name, year_of_issue) values 
 ('Louis Armstrong Plays W.C. Handy',1997),
 ('The Falcon Theatre', 1965),
 ('Electric Ladyland',1968),
-('Still Got The Blues',1990);
+('Still Got The Blues',2020);
 
 insert into artist_album(album_id, artist_id) values
 (1,1),
@@ -32,14 +33,14 @@ insert into track (name , duration, album_id) values
 ('Track_2', 156, 2),
 ('Track_3', 132, 3),
 ('Track_4', 199, 4),
-('Track_5', 125, 2),
-('Track_6', 209, 4);
+('Track_5', 211, 2),
+('My_Track_6', 209, 4);
 
 insert into collection (name, year_of_issue) values
 ('collection_1', 1998),
 ('collection_2', 1970),
 ('collection_3', 1971),
-('collection_4', 1995);
+('collection_4', 2019);
 
 insert into collection_track  (track_id, collection_id) values 
 (1,1),
